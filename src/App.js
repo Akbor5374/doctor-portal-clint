c80b53c6cee41f5734a5264ff0cc41cd7eccd97a
Akbor5374 <akborhossain53741@gmail.com>
2021-04-06 23:57:22 +0600

    branch switch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>
-          AHR Web Dev.
+          AHR Web developer.
         </h3>
       </header>
     </div>
